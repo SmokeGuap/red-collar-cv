0a65d583f9b327e2f04942d0ba55f45d16c20a94
Kirill <mustang.horse34@gmail.com>
2024-02-16 08:55:30 +0700

    add assets and css variables

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,7 @@
+export { ReactComponent as ArrowIcon } from './arrow.svg';
+export { ReactComponent as BlackArrowIcon } from './blackArrow.svg';
+export { ReactComponent as LogoIcon } from './logo.svg';
+export { ReactComponent as PhoneIcon } from './phone.svg';
+export { ReactComponent as MailIcon } from './mail.svg';
+export { ReactComponent as TelegramIcon } from './telegram.svg';
+export { ReactComponent as GithubIcon } from './github.svg';
