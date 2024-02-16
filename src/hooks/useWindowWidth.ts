055a362713a752b09622afd57290f0111f97e954
Kirill <mustang.horse34@gmail.com>
2024-02-16 09:04:19 +0700

    add useWindowWidth hook

---
+++ src/hooks/useWindowWidth.ts
@@ -0,0 +1,22 @@
+import { useLayoutEffect, useState } from 'react';
+
+const useWindowWidth = () => {
+  const [width, setWidth] = useState<number>();
+
+  useLayoutEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return width;
+};
+
+export default useWindowWidth;
